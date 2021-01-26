94fbcd0c0d5e6582be5f4448b45b36f691297e4e
Michael Bryant <shadow53@shadow53.com>
2021-01-25 20:52:21 -0800

    ca-certificates: update to newer version

--- packages/ca-certificates/build.sh
+++ packages/ca-certificates/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://curl.haxx.se/docs/caextract.html
 TERMUX_PKG_DESCRIPTION="Common CA certificates"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=20200722
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=20210125
 TERMUX_PKG_SRCURL=https://curl.haxx.se/ca/cacert.pem
 # If the checksum has changed, it may be time to update the package version:
-TERMUX_PKG_SHA256=2782f0f8e89c786f40240fc1916677be660fb8d8e25dede50c9f6f7b0c2c2178
+TERMUX_PKG_SHA256=e010c0c071a2c79a76aa3c289dc7e4ac4ed38492bfda06d766a80b707ebd2f29
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
