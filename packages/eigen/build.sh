16e64b12a1a98e915ebadb4ef7cf79da5ea95a1b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-03-19 14:45:16 -0700

    and eigen

---
+++ packages/eigen/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=http://eigen.tuxfamily.org
+TERMUX_PKG_DESCRIPTION="Eigen is a C++ template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms"
+TERMUX_PKG_LICENSE="MPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=3.3.7
+TERMUX_PKG_SHA256=d56fbad95abf993f8af608484729e3d87ef611dd85b3380a8bad1d5cbc373a57
+TERMUX_PKG_SRCURL=https://gitlab.com/libeigen/eigen/-/archive/${TERMUX_PKG_VERSION}/eigen-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_BREAKS="eigen-dev"
+TERMUX_PKG_REPLACES="eigen-dev"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_BUILD_TYPE=Release"
+TERMUX_PKG_GROUPS="science"
